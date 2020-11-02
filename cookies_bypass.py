8801597c20889e991d00142e839f1f56dd8a3063
ohyicong <ohyicong@hotmail.com>
2020-11-03 00:17:15 +0800

    minor notification update

--- cookies_bypass.py
+++ cookies_bypass.py
@@ -8,15 +8,15 @@ options = webdriver.ChromeOptions()
 
 #Find your google cookie directory
 path_to_chrome_cookie="user-data-dir=C:\\Users\\OHyic\\AppData\\Local\\Google\\Chrome\\User Data"
-#options.add_argument(path_to_chrome_cookie) #Path to your chrome profile
+#Path to your chrome profile
+options.add_argument(path_to_chrome_cookie) 
 try:
     driver = webdriver.Chrome(os.getcwd()+"\\webdriver\\chromedriver.exe", options=options)
-    
-    #go to the website that you want to bypass
-    driver.get("https://accounts.google.com/signin/v2/identifier?flowName=GlifWebSignIn&flowEntry=ServiceLogin")
 except:
-    print("[-] Please update the chromedriver.exe in the webdriver folder according to your chrome version:https://chromedriver.chromium.org/downloads")
-
+    print("[-] 1. Please close all your chrome browser before opening the program")
+    print("[-] 2. Please update the chromedriver.exe in the webdriver folder according to your chrome version:https://chromedriver.chromium.org/downloads")
+#go to the website that you want to bypass
+driver.get("https://accounts.google.com/signin/v2/identifier?flowName=GlifWebSignIn&flowEntry=ServiceLogin")
 time.sleep(30)
 #dont forget to close your driver, as chrome cookie can only work on one thread.
 driver.close()
